11c400789b2f5f0046d7601e30b62368547834e0
Neven <nserdarusic25@gmail.com>
2023-03-30 18:28:01 +0200

    Gitignore postavke

--- DemoWebShop/DemoWebshop/Settings.cs
+++ DemoWebShop/DemoWebshop/Settings.cs
@@ -1,28 +1,35 @@
 ﻿namespace DemoWebshop
 {
     //KOJI SE SVEPAKETI DODAJU U DEPENDECIES:
-        //Microsoft.AspNetCore.Identity.EntityFrameworkCore
-        //Microsoft.AspNetCore.Identity.
-        //Microsoft.EntityFrameworkCore.SqlServer
-        //Microsoft.EntityFrameworkCore.Tools
-        //Microsoft.VisualStudio.Web.CodeGeneration.Design
+    //Microsoft.AspNetCore.Identity.EntityFrameworkCore
+    //Microsoft.AspNetCore.Identity --> ovaj paket generira ostala 4 paketa
+    //Microsoft.EntityFrameworkCore.SqlServer
+    //Microsoft.EntityFrameworkCore.Tools
+    //Microsoft.VisualStudio.Web.CodeGeneration.Design
 
     //AREAS FOLDER:
-        //Identity
-            //Data
-                //ApplicationDbContext.cs
-                //ApplicationUser.cs
-            //Pages
-                //Account
+    //Identity
+    //Data
+    //ApplicationDbContext.cs
+    //ApplicationUser.cs
+    //Pages
+    //Account
 
-    
-    
+    //============================================================================================================
+
     //KORACI KOJI SLIJEDE:
-    //Dodavanje partial View-a od Identifikacije u Views/Shared/Layout.cshtml (Register/Login)
 
-    //Kreiranje servisa za korištenje RazorPage opcija unutar Program.cs ---> builder.Services.AddRazorPages();
+    //Dodavanje partial View-a Identifikacije ----- Views/Shared/Layout.cshtml (Register/Login)
 
-    //Mapiranje RazorPage stranica unutar Program.cs ---> app.MapRazorPages();
+    //Kreiranje servisa za korištenje RazorPage opcija ----- Program.cs ---> builder.Services.AddRazorPages();
 
+    //Mapiranje RazorPage stranica ---- Program.cs ---> app.MapRazorPages();
 
+    //Brisanje generiranog Connection stringa iz datoteke ---- appsettings.json (sakrivamo povjerljive podatke)
+
+    //Stvaranje novog Connection stringa ----appsettings.Development.json ---> "Server=localhost\\sqlexpress; Database=m9_webshop; Integrated Security=true; TrustServerCertificate=true; MultipleActiveResultSets=true;" (u ovoj datoteci ostaje Connection string LOKALNO, ne ide na Github)
+
+    //Izbacivanje gornje datoteke iz projekta ---- .gitIgnore (LocalDisc C: > users > anama > source > modul_9)  (drag and drop u VS) ---> appsettings.Development.json
+
+    //
 }
