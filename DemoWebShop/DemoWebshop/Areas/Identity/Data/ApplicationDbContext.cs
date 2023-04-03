1db96f75d73a2409499abf76b93a2fcc5d352235
Neven <nserdarusic25@gmail.com>
2023-04-03 13:48:04 +0200

    Dodan seeding za Kategorije i Produkti

--- DemoWebShop/DemoWebshop/Areas/Identity/Data/ApplicationDbContext.cs
+++ DemoWebShop/DemoWebshop/Areas/Identity/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using DemoWebshop.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace DemoWebshop.Data;
 
@@ -37,5 +38,26 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+
+
+        Product product01 = new Product() { Id= 1,  Title = "Jabuka crvena", Description = "Crvena jabuka", Sku = "j01-voće", InStock = 1023.13M, Price = 0.77M, Image = "crvenajabuka.png" };
+        Product product02 = new Product() { Id = 2, Title = "Jabuka zelena", Description = "Zelena jabuka", Sku = "j02-voće", InStock = 957.15M, Price = 0.73M, Image = "zelenajabuka.png" };
+        Product product03 = new Product() { Id = 17, Title = "Jogurt 0,5L", Description = "Jogurt sa 2,8% mm", Sku = "j03-jog", InStock = 500M, Price = 1.22M, Image = "jogurt2,8%.png" };
+        Product product04 = new Product() { Id = 54, Title = "Pivo 0,33L", Description = "Bavaria", Sku = "p17-pića", InStock = 257M, Price = 2.14M, Image = "bavaria.png" };
+        Product product05 = new Product() { Id = 22, Title = "Kruh Klara", Description = "Kruh sa sjemenkama", Sku = "p09-peka", InStock = 150M, Price = 1.27M, Image = "kruhsjemnkeKlara.png" };
+
+        
+
+
+        Category mliječniProizvodi = new Category() { Id = 10, Title = "Mliječni proizvodi", Description = "Mlijeko i mliječni proizvodi", Image = "mlijecniproizvodi.png" };
+        Category voće = new Category() { Id = 1, Title = "Voće", Description = "Voće i proizvodi od voća", Image = "voce.png" };
+        Category pekarskiProizvodi = new Category() { Id = 20, Title = "Pekarski proizvodi", Description = "Proizvodi od brašna", Image = "pekarskiproizvodi.png" };
+        Category pića = new Category() { Id = 50, Title = "Pića", Description = "Pića i napitci", Image = "pica.png" };
+        Category meso = new Category() { Id = 70, Title = "Meso", Description = "Meso i proizvodi od mesa", Image = "meso.png" };
+
+
+        builder.Entity<Product>().HasData(product01, product02, product03, product04, product05);
+        builder.Entity<Category>().HasData(mliječniProizvodi, voće, pekarskiProizvodi, pića, meso);
+
     }
 }
---
+++ DemoWebShop/DemoWebshop/Migrations/20230403114731_DataSeedingCategoryAndProduct.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoWebshop.Migrations
+{
+    public partial class DataSeedingCategoryAndProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Image", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Voće i proizvodi od voća", "voce.png", "Voće" },
+                    { 10, "Mlijeko i mliječni proizvodi", "mlijecniproizvodi.png", "Mliječni proizvodi" },
+                    { 20, "Proizvodi od brašna", "pekarskiproizvodi.png", "Pekarski proizvodi" },
+                    { 50, "Pića i napitci", "pica.png", "Pića" },
+                    { 70, "Meso i proizvodi od mesa", "meso.png", "Meso" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Image", "InStock", "Price", "Sku", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Crvena jabuka", "crvenajabuka.png", 1023.13m, 0.77m, "j01-voće", "Jabuka crvena" },
+                    { 2, "Zelena jabuka", "zelenajabuka.png", 957.15m, 0.73m, "j02-voće", "Jabuka zelena" },
+                    { 17, "Jogurt sa 2,8% mm", "jogurt2,8%.png", 500m, 1.22m, "j03-jog", "Jogurt 0,5L" },
+                    { 22, "Kruh sa sjemenkama", "kruhsjemnkeKlara.png", 150m, 1.27m, "p09-peka", "Kruh Klara" },
+                    { 54, "Bavaria", "bavaria.png", 257m, 2.14m, "p17-pića", "Pivo 0,33L" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 50);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 70);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 54);
+        }
+    }
+}
